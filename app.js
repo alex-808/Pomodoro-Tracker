1b378eafca210da5e4ede18f2ae0e18e0a62cd85
Alex Davis <acdavis050@gmail.com>
2020-12-19 20:42:46 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// accept an input time
+// update UI with set time
+// use setTimer to create a function to countdown timer which updates the UI value
+// when timer hits 0, execute alarm
