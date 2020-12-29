64161ca68b48a1833debef85c91d59b94ddf11d2
Alex Davis <acdavis050@gmail.com>
2020-12-28 21:19:51 -0800

    Added timer type selection and timer start/stop condition events

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ const generateWaterCBs = function (size, goal) {
 // break timer
 
 // pom timers
-const pomForm = document.querySelector('.pomForm');
+const pomTimerForm = document.querySelector('.pomTimerForm');
 const pomDurationField = document.querySelector('.pomDuration');
 const breakDurationField = document.querySelector('.breakDuration');
 const pomSubmit = document.querySelector('.pomSubmit');
@@ -74,47 +74,97 @@ let breakDuration = 0;
 const currentTimeDiv = document.querySelector('.currentTime');
 const timerToggleBtn = document.querySelector('.timerToggle');
 const timerResetBtn = document.querySelector('.timerReset');
+const selectPomBtn = document.querySelector('.selectPom');
+const selectBreakBtn = document.querySelector('.selectBreak');
+const pomCounterDiv = document.querySelector('.pomCounter');
+const breakCounterDiv = document.querySelector('.breakCounter');
 let currentTime;
 let timerActive = false;
 let timer;
-
-pomSubmit.addEventListener('click', function (e) {
-    e.preventDefault();
-    // pomDuration = pomDurationField.value;
-    // breakDuration = breakDurationField.value;
-    pomDuration = 50;
-    breakDuration = 10;
-    currentTime = pomDuration;
-    currentTimeDiv.textContent = currentTime;
-});
+let timerType = 'pomodoro';
+let breakCounter = 0;
+let pomodoroCounter = 0;
 
 const startTimer = function () {
     timer = setInterval(function () {
         currentTime--;
         currentTimeDiv.textContent = currentTime;
-    }, 1000);
+
+        if (currentTime === 0) {
+            stopTimer();
+            timerActive = false;
+            if (timerType === 'pomodoro') {
+                pomodoroCounter++;
+                pomCounterDiv.textContent = pomodoroCounter;
+            } else {
+                breakCounter++;
+                breakCounterDiv.textContent = breakCounter;
+            }
+        }
+    }, 100);
 };
 
 const stopTimer = function () {
     clearInterval(timer);
 };
 
-timerToggleBtn.addEventListener('click', function (e) {
+pomSubmit.addEventListener('click', function (e) {
     e.preventDefault();
 
-    if (timerActive) stopTimer();
-    else startTimer();
+    // should check for null values
+
+    pomDuration = pomDurationField.value;
+    breakDuration = breakDurationField.value;
+    // pomDuration = 50;
+    // breakDuration = 10;
+    currentTime = pomDuration;
+    currentTimeDiv.textContent = currentTime;
+});
+
+timerResetBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    currentTime = pomDuration;
+    currentTimeDiv.textContent = currentTime;
+});
+
+timerToggleBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    // fix button text when time has run out
+    if (timerActive) {
+        timerToggleBtn.textContent = 'Start';
+        stopTimer();
+    } else if (currentTime !== 0) {
+        timerToggleBtn.textContent = 'Stop';
+        startTimer();
+    }
     timerActive = timerActive ? false : true;
 });
 
-// break tracker
+selectPomBtn.addEventListener('click', function (e) {
+    e.preventDefault();
 
-// dark mode
+    timerType = 'pomodoro';
+    currentTime = pomDuration;
+    currentTimeDiv.textContent = currentTime;
+});
+
+selectBreakBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    timerType = 'break';
+    currentTime = breakDuration;
+    currentTimeDiv.textContent = currentTime;
+});
+
+// pom/break trackers
 
 // pom tracker
 // receives goal poms
 
 // git timer
+
+// dark mode
 
 // to do list
 
