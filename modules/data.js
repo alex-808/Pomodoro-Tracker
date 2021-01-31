1d41bec044630f448eeebd7751896b4468f40e89
Alex Davis <acdavis050@gmail.com>
2021-01-30 18:49:39 -0800

    Added functionality to settings.html reset buttons

--- modules/data.js
+++ modules/data.js
@@ -1,3 +1,28 @@
+const defaultSettings = {
+    waterSettings: {
+        bottleSize: 25,
+        waterGoal: 124,
+        bottleGoal: 5,
+    },
+    pomSettings: {
+        pomDuration: 50,
+        breakDuration: 10,
+        pomGoal: 7,
+        breakGoal: 7,
+    },
+    commitSettings: {
+        commitFrequency: 30,
+        commitGoal: 5,
+        commitDisabled: false,
+    },
+    updatefromLocalStorage() {
+        const data = JSON.parse(localStorage.settings);
+        for (let [key, value] of Object.entries(data)) {
+            this[`${key}`] = value;
+        }
+    },
+};
+
 const settings = {
     waterSettings: {
         bottleSize: 25,
@@ -75,4 +100,4 @@ const setupData = function () {
 
 setupData();
 
-export { settings, userData, timerState, updateLocalStorage };
+export { settings, defaultSettings, userData, timerState, updateLocalStorage };
